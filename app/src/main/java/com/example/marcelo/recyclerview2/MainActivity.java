ce05c9422a7d0b95f28c720f58dab99c56e26780
Marcelo Goncalves <tchelojg@gmail.com>
2018-04-12 23:26:07 -0300

    finalizando projeto

--- app/src/main/java/com/example/marcelo/recyclerview2/MainActivity.java
+++ app/src/main/java/com/example/marcelo/recyclerview2/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.marcelo.recyclerview2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
